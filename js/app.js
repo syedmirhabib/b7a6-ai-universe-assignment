1ef99a9a26ddda7c5e4f19ec516df5c2a8a91802
Syed Mir Habib <mirakik.habib@gmail.com>
2023-03-04 13:39:04 +0600

    Accuracy formate; text and bg color change

--- js/app.js
+++ js/app.js
@@ -209,9 +209,9 @@ loaderActive(false);
                   </p>
               </div>
   
-              <p class="absolute top-6 right-6 w-max bg-red-400 py-1 px-2 rounded-md text-white ${
+              <p class="absolute top-6 right-6 w-max bg-green-500 py-1 px-2 rounded-md text-black ${
                 data.accuracy.score === null ? "hidden" : "block"
-              } "> ${(data.accuracy.score * 100).toFixed(1) + "% accuracy"} </p>
+              } "> ${"Accuracy:"+" "+(data.accuracy.score * 100).toFixed(1) + "%"} </p>
 
           </div>
           </div>
